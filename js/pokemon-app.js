5cdf2e0fcf726672a48444c8e21c05c7a3c80b49
HenryPereira <56375668+HenryPereira@users.noreply.github.com>
2022-04-25 16:56:24 -0500

    display clues, 'new clue' button, points display

--- js/pokemon-app.js
+++ js/pokemon-app.js
@@ -1,19 +1,22 @@
-const MAP_SIZE = 500
-const NU_CENTER = ol.proj.fromLonLat([-87.6753, 42.056])
+const MAP_SIZE = 500;
+const NU_CENTER = ol.proj.fromLonLat([-87.6753, 42.056]);
 
-const AUTOMOVE_SPEED = 1
-const UPDATE_RATE = 100
+const AUTOMOVE_SPEED = 1;
+const UPDATE_RATE = 100;
 
-let landmarkCount = 0
-let clueIndex = 1
-let clueList = [["Placeholder clue for The Rock", 0]]
-clueMessage = clueList[0][0]
+const LANDMARK_POINTS = [10, 5, 2]
+
+let landmarkCount = 0;
+let clueIndex = 1;
+let clueList = [];
+let cluesGiven = 0;
+
 
 
 let gameState = {
 	points: 0,
 	captured: [],
-	messages: "Clue: " + clueMessage,
+	messages: [],
 }
 
 let map = new InteractiveMap({
@@ -23,8 +26,8 @@ let map = new InteractiveMap({
 
 	initializeMap() {
 		this.loadLandmarks("landmarks-scavenger-hunt", (landmark) => {
-			return true
-    })
+			return true;
+    	})
 	},
 
 	update() {
@@ -32,23 +35,26 @@ let map = new InteractiveMap({
 
 	initializeLandmark: (landmark, isPlayer) => {
 		if (landmark.openMapData) {
-			landmark.name = landmark.openMapData.name
+			landmark.name = landmark.openMapData.name;
 		}
 		landmark.idNumber = landmarkCount++
-    if (!landmark.idNumber == 0) {
-      clueList.push([landmark.name, landmark.idNumber])
-    }
-    return landmark
+		if (!landmark.idNumber == 0) {
+			clueList.push(landmark.openMapData.hints);
+		}
+		console.log("Initializing", landmark.name);
+		console.log(clueList);
+		return landmark;
 	}, 
 
 	onEnterRange: (landmark, newLevel, oldLevel, dist) => {
 		console.log("enter", landmark.name, newLevel)
 		if (newLevel == 2) {
-			if (!gameState.captured.includes(landmark.name) && landmark.idNumber == clueIndex) {
-        gameState.points += landmark.points
-        clueIndex++
-        gameState.messages = "Placeholder clue for " + clueList[clueIndex][0]
-				gameState.captured.push(landmark.name)
+			if (!gameState.captured.includes(landmark.name) && landmark.idNumber - 1 == clueIndex) {
+				gameState.points += LANDMARK_POINTS[cluesGiven - 1];
+				clueIndex++;
+				gameState.messages = [clueList[clueIndex][0]];
+				gameState.captured.push(landmark.name);
+				cluesGiven = 1;
 			}
 		}
 	},
@@ -64,18 +70,26 @@ let map = new InteractiveMap({
 				noBG: true 
 			}
 		}
-    let hue = landmark.idNumber*30
-    console.log(hue)
-		return {
-			label: landmark.name + "\n" + landmark.distanceToPlayer +"m",
-			fontSize: 8,
-			icon: "person_pin_circle",
-      iconColor: [hue, .5, .6],
-			bgColor: [hue, 1, .2],
-			noBG: true 
-		}
+		let hue = landmark.idNumber*30
+		console.log(hue)
+			return {
+				label: landmark.name + "\n" + landmark.distanceToPlayer +"m",
+				fontSize: 8,
+				icon: "person_pin_circle",
+				iconColor: [hue, .5, .6],
+				bgColor: [hue, 1, .2],
+				noBG: true 
+			}
 	},
 })
+
+function newClue() {
+	console.log(cluesGiven);
+	if(cluesGiven < 3) {
+		gameState.messages.push(clueList[clueIndex][cluesGiven]);
+		cluesGiven += 1;
+	}
+}
 
 window.onload = (event) => {
 	const app = new Vue({
@@ -83,8 +97,16 @@ window.onload = (event) => {
 		<div id="app">
 		<header></header>
 			<div id="main-columns">
-				<div class="main-column" style="flex:1;overflow:scroll;max-height:200px">
-					{{gameState.messages}} <br> You already found: {{gameState.captured}} 
+				<div class="main-column" style="flex:1;overflow:scroll;max-height:300px">
+					<button onclick="newClue()">New Clue</button>
+					<p> Clues: </p>
+					<ol>
+						<li v-for="message in gameState.messages">
+							{{message}}
+						</li>
+					</ol>
+					<p> You already found: {{gameState.captured}} </p>
+					<p> You have: {{gameState.points}} points </p>
 				</div>
 				<div class="main-column" style="overflow:hidden;width:${MAP_SIZE}px;height:${MAP_SIZE}px">
 					<location-widget :map="map" />
@@ -102,5 +124,5 @@ window.onload = (event) => {
 			"location-widget": locationWidget,
 		}),
 		el: "#app"
-	})
+	});
 };
